f2d985bc3bbed3fd0a0092d3ae5847d5f6d4e3d8
citrice <31527865+citrice@users.noreply.github.com>
2017-09-03 10:22:03 -0700

    First challenge
    Added first challenge to github :+1:

---
+++ challenge1.py
@@ -0,0 +1,9 @@
+name = input("what is ur mane M8?")
+
+age = input("how old r u fam?")
+
+username = input("wat is ur username?")
+
+answre = "your name is %s, your age is %s, your username is %s." % (name, age, username)
+
+print(answre)
